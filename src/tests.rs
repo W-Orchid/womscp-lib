7ec38c9a60e318ace2ed43f86f12b46cf7fde522
Rumen Mitov <108842741+rumenmitov@users.noreply.github.com>
2024-10-29 11:20:45 +0100

    feat: response handling (#5)

--- src/tests.rs
+++ src/tests.rs
@@ -1,11 +1,11 @@
 #[cfg(test)] 
-use crate::womscp::Request;
+use crate::womscp::*;
 
 #[test]
 fn parse_request() {
-    let buf = [1, 0, 0x0d, 5, 3, 0, 0, 0, 0x7b, 0];
-    let correct_res = Request {
-        version: 1,
+    let buf :[u8; WOMSCP_REQ_LEN] = [WOMSCP_VERSION, 0, 0x0d, 5, 3, 0, 0, 0, 0x7b, 0];
+    let correct_req = Request {
+        version: WOMSCP_VERSION,
         m_id: 0x0d,
         s_id: 5,
         sensor_type: 3,
@@ -13,7 +13,27 @@ fn parse_request() {
         flags: 0
     };
 
-    let res :Request = Request::try_from(buf).expect("Couldn't parse byte string!");
+    let req :Request = Request::try_from(buf).expect("Couldn't parse byte string!");
+
+    assert_eq!(req, correct_req);
+}
+
+#[test]
+fn wrong_version() {
+    let buf :[u8; WOMSCP_REQ_LEN] = [0, 0, 0x0d, 5, 3, 0, 0, 0, 0x7b, 0];
+    let correct_res = Response::new(Err(ResponseError::Version));
+
+    let mut res = Response::new(Ok(()));
+
+    match Request::try_from(buf) {
+        Ok(_) => panic!("Wrong version not detected!"),
+        Err(e) => {
+            match e {
+                ResponseError::Version => res.response = Err(ResponseError::Version),
+                _ => panic!("Wrong version not detected! Some other error occured!")
+            }
+        }
+    }
 
     assert_eq!(res, correct_res);
 }
