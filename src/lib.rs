ecd11db0916d520a2d77edca198a316e5aca6bb3
Rumen <rumenmitov@protonmail.com>
2024-10-29 12:23:27 +0100

    feat: simplifying lib api

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,6 @@ pub mod womscp {
 
     pub const WOMSCP_VERSION :u8 = 1;
     pub const WOMSCP_REQ_LEN :usize = 10;
-    pub const WOMSCP_RES_LEN :usize = 2;
 
 
     #[derive(Debug)]
@@ -25,7 +24,7 @@ pub mod womscp {
     }
 
     impl TryFrom<[u8; WOMSCP_REQ_LEN]> for Request {
-        type Error = ResponseError;
+        type Error = Response;
 
         fn try_from(buf: [u8; WOMSCP_REQ_LEN]) -> Result<Self, Self::Error> {
             let req = Request { 
@@ -38,7 +37,7 @@ pub mod womscp {
             };
 
             if req.version != WOMSCP_VERSION {
-                return Err(ResponseError::Version);
+                return Err(Response::Version);
             }
 
             Ok(req)
@@ -46,20 +45,20 @@ pub mod womscp {
     }
 
     impl TryFrom<TcpStream> for Request {
-        type Error = ResponseError;
+        type Error = Response;
 
         fn try_from(mut stream: TcpStream) -> Result<Self, Self::Error> {
             let mut buf :[u8; WOMSCP_REQ_LEN] = [0; WOMSCP_REQ_LEN];
 
             if let Err(e) = stream.read(&mut buf) {
                 eprintln!("{:?}", e);
-                return Err(ResponseError::Tcp);
+                return Err(Response::Tcp);
             }
 
             let req =  Self::try_from(buf)?;
 
             if req.version != WOMSCP_VERSION {
-                return Err(ResponseError::Version);
+                return Err(Response::Version);
             }
 
             return Ok(req);
@@ -67,8 +66,37 @@ pub mod womscp {
     }
 
 
+    impl TryInto<[u8; WOMSCP_REQ_LEN]> for Request {
+        type Error = Response;
+
+        fn try_into(self) -> Result<[u8; WOMSCP_REQ_LEN], Self::Error> {
+            let mut buf :[u8; WOMSCP_REQ_LEN] = [0; WOMSCP_REQ_LEN];
+
+            let m_id_bytes = self.m_id.to_be_bytes();
+            let data_bytes = self.data.to_be_bytes();
+
+            if self.version != WOMSCP_VERSION {
+                return Err(Response::Version);
+            }
+
+            buf[0] = self.version;
+            buf[1] = m_id_bytes[0];
+            buf[2] = m_id_bytes[1];
+            buf[3] = self.s_id;
+            buf[4] = self.sensor_type;
+            buf[5] = data_bytes[0];
+            buf[6] = data_bytes[1];
+            buf[7] = data_bytes[2];
+            buf[8] = data_bytes[3];
+            buf[9] = self.flags;
+
+            Ok(buf)
+        }
+    }
+
+
     #[derive(Debug, PartialEq)]
-    pub enum ResponseError {
+    pub enum Response {
         NotReady = 1,
         Version,
         Unrecognised,
@@ -76,41 +104,6 @@ pub mod womscp {
         Database
     }
 
-    #[derive(Debug, PartialEq)]
-    pub struct Response {
-        pub version :u8,
-        pub response :Result<(), ResponseError>
-    }
-
-    impl Response {
-        pub fn new(res :Result<(), ResponseError>) -> Self {
-            Response {
-                version: WOMSCP_VERSION,
-                response: res
-            }
-        }
-    }
-
-    impl Into<[u8; WOMSCP_RES_LEN]> for Response {
-        fn into(self) -> [u8; WOMSCP_RES_LEN] {
-            let mut buf = [0; WOMSCP_RES_LEN];
-
-            buf[0] = self.version;
-            buf[1] = if self.response.is_ok() {
-                0
-            } else {
-                match self.response.unwrap_err() {
-                    ResponseError::NotReady     => 1,
-                    ResponseError::Version      => 2,
-                    ResponseError::Unrecognised => 3,
-                    ResponseError::Tcp          => 4,
-                    ResponseError::Database     => 5
-                }
-            };
-
-            buf
-        }
-    }
 }
 
 
