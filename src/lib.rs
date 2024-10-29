41a94de7f888c7abc30536774f0315f0bb66e9e1
Rumen <rumenmitov@protonmail.com>
2024-10-29 09:37:29 +0100

    misc: separate tests.rs file

--- src/lib.rs
+++ src/lib.rs
@@ -83,24 +83,5 @@ pub mod womscp {
     }
 }
 
-#[cfg(test)] 
-mod tests {
-    use crate::womscp::Request;
 
-    #[test]
-    fn parse_request() {
-        let buf = [1, 0, 0x0d, 5, 3, 0, 0, 0, 0x7b, 0];
-        let correct_res = Request {
-            version: 1,
-            m_id: 0x0d,
-            s_id: 5,
-            sensor_type: 3,
-            data: 0x7b,
-            flags: 0
-        };
-
-        let res :Request = Request::try_from(buf).expect("Couldn't parse byte string!");
-
-        assert_eq!(res, correct_res);
-    }
-}
+mod tests;
