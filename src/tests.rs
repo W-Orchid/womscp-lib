cca9796829a36fa3cfbf67881692478bd6b619e0
Rumen <rumenmitov@protonmail.com>
2024-11-02 19:12:41 +0100

    feat: making api more flexible

--- src/tests.rs
+++ src/tests.rs
@@ -13,7 +13,7 @@ fn parse_buf_to_request() {
         flags: 0
     };
 
-    let req :Request = Request::try_from(buf).expect("Couldn't parse byte string!");
+    let req :Request = Request::try_from(&buf).expect("Couldn't parse byte string!");
 
     assert_eq!(req, correct_req);
 }
@@ -31,7 +31,7 @@ fn parse_request_to_buf() {
 
     let correct_buf :[u8; WOMSCP_REQ_LEN] = [WOMSCP_VERSION, 0, 0x0d, 5, 3, 0, 0, 0, 0x7b, 0];
 
-    let buf :[u8; WOMSCP_REQ_LEN] = req.try_into().unwrap();
+    let buf :[u8; WOMSCP_REQ_LEN] = (&req).try_into().unwrap();
 
     assert_eq!(buf, correct_buf);
 }
@@ -40,7 +40,7 @@ fn parse_request_to_buf() {
 fn wrong_version() {
     let buf :[u8; WOMSCP_REQ_LEN] = [0, 0, 0x0d, 5, 3, 0, 0, 0, 0x7b, 0];
 
-    match Request::try_from(buf) {
+    match Request::try_from(&buf) {
         Ok(_) => panic!("Wrong version not detected!"),
         Err(e) => {
             match e {
